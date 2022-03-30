a5257ee346fde5da7a10371845c052ce57ce23aa
Cailepaquemebaile <mateo2523299@gmail.com>
2022-03-30 16:45:51 -0500

    Promesas

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,29 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.get('/', (req,res) =>{
-    res.send('Prueba Servidor')
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/prueba/:id', async (req,res,next) =>{
+    console.log('antes de la promesa')
+    let x =10;
+    const promesa = new Promise((resolve,reject)=>{
+        if (x==10){
+            resolve('la promesa se resuelve')
+        }else{
+            reject('No se cumple')
+        }
+    });
+
+    await promesa.then((respuesta)=>{
+        console.log(respuesta)
+    }).catch(err=>{
+        console.log(err)
+    })
+
+    console.log('despues de la promesa')
+    res.status(200).send('Prueba Servidor')
 })
 app.listen(port, () => {
     return console.log(`Servidor corriendo en el puerto ${port}`)
